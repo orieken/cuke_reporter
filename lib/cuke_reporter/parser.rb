7cd9eafac55544bc6382cd872780ebe946e6e979
Jarod Adair <adairjk@gmail.com>
2014-09-24 19:57:48 -0500

    update parser.rb to capture scenario.status

--- lib/cuke_reporter/parser.rb
+++ lib/cuke_reporter/parser.rb
@@ -26,11 +26,18 @@ module CukeReporter
     def get_result
       results = []
 
-      @feature.scenarios.each do |scenarios|
-        scenarios.steps.each do |step|
+      @feature.scenarios.each do |scenario|
+        scenario_results = []
+        scenario.steps.each do |step|
+          scenario_results << step.result.status
           results << step.result.status
         end
+        scenario.status = get_status(scenario_results)
       end
+      get_status(results)
+    end
+
+    def get_status(results)
 
       if results.include?('failed')
         'failed'
